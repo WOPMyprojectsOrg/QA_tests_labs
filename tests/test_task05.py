d7fd149531840448dd41387580ee4bd85a5defe2
Елисей Чубукин <e-chubukin2017@yandex.ru>
2025-10-10 10:55:17 +0700

    test05

--- tests/test_task05.py
+++ tests/test_task05.py
@@ -1,1 +1,9 @@
 # Напишите тесты для функции is_palindrome(s)
+
+def test_task05():
+    from tasks.task05 import polynom
+    assert polynom('A man a plan a canal Panama') == True
+    assert polynom('baclajan') == False
+    assert polynom(1232432423) == False
+    assert polynom(123321) == True
+    assert polynom() == True
