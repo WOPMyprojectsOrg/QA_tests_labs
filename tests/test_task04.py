6622cd436ab9c94625f692abba66bd50dcc5fafa
MudryMistichecky <ez792953@gmail.com>
2025-10-11 09:13:17 +0700

    написал тест для функции max_of_three()

--- tests/test_task04.py
+++ tests/test_task04.py
@@ -1,1 +1,5 @@
 # Напишите тесты для функции max_of_three(a, b, c)
+def test_max_of_three():
+    from tasks.task04 import max_of_three()
+    assert max_of_three(-1, -2, -3) == -1
+    assert max_of_three(0, 1, 20) == 20
