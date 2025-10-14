13d1b1618931b3005a4222975c68a9e9a0916ab0
Елисей Чубукин <e-chubukin2017@yandex.ru>
2025-10-14 19:54:14 +0700

    task06

--- tasks/task06.py
+++ tasks/task06.py
@@ -1,2 +1,15 @@
 # Задача: Напишите функцию, которая возвращает список всех простых чисел до n (включительно).
 # Простое число — делится только на 1 и на себя.
+
+def primes_up_to(n):
+    primes = [1]
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break         
+        if is_prime:
+            primes.append(num)
+    
+    return primes
